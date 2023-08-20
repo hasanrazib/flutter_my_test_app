8e7d2e37258c52835b539f81b2275f1b9025ab81
Razib Hasan <razib.hasan@dnet.org.bd>
2023-08-21 00:16:52 +0600

    spliting code across files

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget{
+
+  const GradientContainer({super.key});
+  
+  @override
+  Widget build( context) {
+   return  Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.amber,
+              Colors.red],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+              ),
+            ),
+      child: const Center(
+        child: Text('Custome Widget build'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_test_app/gradient_container.dart';
 
 void main() {
   runApp(const MaterialApp(
       home: Scaffold(
     body:GradientContainer(),
   )));
-}
-
-class GradientContainer extends StatelessWidget{
-
-  const GradientContainer({super.key});
-  
-  @override
-  Widget build( context) {
-   return  Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.amber,
-              Colors.red],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-              ),
-            ),
-      child: const Center(
-        child: Text('Custome Widget build'),
-      ),
-    );
-  }
 }
