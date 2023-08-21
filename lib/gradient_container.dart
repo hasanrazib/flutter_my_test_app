1dbd11a955e00f111fba3a5f06cc2c37a4c222db
Razib Hasan <razib.hasan@dnet.org.bd>
2023-08-21 11:43:30 +0600

    introducing variable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+var alignmentStart = Alignment.topLeft;
+var alignmentEnd = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget{
 
   const GradientContainer({super.key});
@@ -7,13 +10,13 @@ class GradientContainer extends StatelessWidget{
   @override
   Widget build( context) {
    return  Container(
-      decoration: const BoxDecoration(
+      decoration:  BoxDecoration(
           gradient: LinearGradient(
-            colors: [
+            colors: const [
               Colors.amber,
               Colors.red],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
+              begin: alignmentStart,
+              end: alignmentEnd
               ),
             ),
       child: const Center(
